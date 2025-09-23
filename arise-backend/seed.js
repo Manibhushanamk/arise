15ff01f4bf89419c5d7b1a8119b6739449324318
Manibhushanamk <manibhushanam4k@gmail.com>
2025-09-23 23:14:48 +0530

    fix: Correct syntax error in seed.js

--- arise-backend/seed.js
+++ arise-backend/seed.js
@@ -65,97 +65,97 @@ const skillResourcesData = [
         skillName: "JavaScript",
         youtubeLink: "https://www.youtube.com/watch?v=PkZNo7MFNFg",
         docsLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-        practiceLink": "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/"
+        practiceLink: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/"
     },
     {
         skillName: "MongoDB",
         youtubeLink: "https://www.youtube.com/watch?v=oSIv-E60NiU",
-        docsLink": "https://www.mongodb.com/docs/",
-        practiceLink": "https://www.w3schools.com/mongodb/"
+        docsLink: "https://www.mongodb.com/docs/",
+        practiceLink: "https://www.w3schools.com/mongodb/"
     },
     {
         skillName: "TensorFlow",
         youtubeLink: "https://www.youtube.com/watch?v=tPYj3fFJGjk",
-        docsLink": "https://www.tensorflow.org/learn",
-        practiceLink": "https://www.kaggle.com/learn/intro-to-machine-learning"
+        docsLink: "https://www.tensorflow.org/learn",
+        practiceLink: "https://www.kaggle.com/learn/intro-to-machine-learning"
     },
     {
         skillName: "HTML",
         youtubeLink: "https://www.youtube.com/watch?v=kUMe1FH4CHE",
-        docsLink": "https://developer.mozilla.org/en-US/docs/Web/HTML",
-        practiceLink": "https://www.w3schools.com/html/"
+        docsLink: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+        practiceLink: "https://www.w3schools.com/html/"
     },
     {
         skillName: "CSS",
         youtubeLink: "https://www.youtube.com/watch?v=OXGznpKZ_sA",
-        docsLink": "https://developer.mozilla.org/en-US/docs/Web/CSS",
-        practiceLink": "https://www.freecodecamp.org/learn/responsive-web-design/"
+        docsLink: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+        practiceLink: "https://www.freecodecamp.org/learn/responsive-web-design/"
     },
     {
         skillName: "React",
         youtubeLink: "https://www.youtube.com/watch?v=bMknfKXIFA8",
-        docsLink": "https://react.dev/learn",
-        practiceLink": "https://scrimba.com/learn/learnreact"
+        docsLink: "https://react.dev/learn",
+        practiceLink: "https://scrimba.com/learn/learnreact"
     },
     {
         skillName: "Node.js",
         youtubeLink: "https://www.youtube.com/watch?v=f2EqECiTBL8",
-        docsLink": "https://nodejs.org/en/docs",
-        practiceLink": "https://nodeschool.io/"
+        docsLink: "https://nodejs.org/en/docs",
+        practiceLink: "https://nodeschool.io/"
     },
     {
-      "skillName": "Express",
-      "youtubeLink": "https://www.youtube.com/watch?v=SccSCuHhOw0",
-      "docsLink": "https://expressjs.com/",
-      "practiceLink": "https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express"
+      skillName: "Express",
+      youtubeLink: "https://www.youtube.com/watch?v=SccSCuHhOw0",
+      docsLink: "https://expressjs.com/",
+      practiceLink: "https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express"
     },
     {
-      "skillName": "REST APIs",
-      "youtubeLink": "https://www.youtube.com/watch?v=GZvSYJDk-us",
-      "docsLink": "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status",
-      "practiceLink": "https://reqres.in/"
+      skillName: "REST APIs",
+      youtubeLink: "https://www.youtube.com/watch?v=GZvSYJDk-us",
+      docsLink: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status",
+      practiceLink: "https://reqres.in/"
     },
     {
-      "skillName": "Python",
-      "youtubeLink": "https://www.youtube.com/watch?v=_uQrJ0TkZlc",
-      "docsLink": "https://docs.python.org/3/",
-      "practiceLink": "https://www.hackerrank.com/domains/python"
+      skillName: "Python",
+      youtubeLink: "https://www.youtube.com/watch?v=_uQrJ0TkZlc",
+      docsLink: "https://docs.python.org/3/",
+      practiceLink: "https://www.hackerrank.com/domains/python"
     },
     {
-      "skillName": "ML Basics",
-      "youtubeLink": "https://www.youtube.com/watch?v=i_LwzRVP7bg",
-      "docsLink": "https://developers.google.com/machine-learning/crash-course",
-      "practiceLink": "https://www.kaggle.com/learn/intro-to-machine-learning"
+      skillName: "ML Basics",
+      youtubeLink: "https://www.youtube.com/watch?v=i_LwzRVP7bg",
+      docsLink: "https://developers.google.com/machine-learning/crash-course",
+      practiceLink: "https://www.kaggle.com/learn/intro-to-machine-learning"
     },
     {
-      "skillName": "Data Analysis",
-      "youtubeLink": "https://www.youtube.com/watch?v=r-uOLxNrNk8",
-      "docsLink": "https://pandas.pydata.org/docs/",
-      "practiceLink": "https://www.datacamp.com/courses/intro-to-python-for-data-science"
+      skillName: "Data Analysis",
+      youtubeLink: "https://www.youtube.com/watch?v=r-uOLxNrNk8",
+      docsLink: "https://pandas.pydata.org/docs/",
+      practiceLink: "https://www.datacamp.com/courses/intro-to-python-for-data-science"
     },
     {
-      "skillName": "Figma",
-      "youtubeLink": "https://www.youtube.com/watch?v=3q3FV65JDBg",
-      "docsLink": "https://help.figma.com/hc/en-us",
-      "practiceLink": "https://www.figma.com/community"
+      skillName: "Figma",
+      youtubeLink: "https://www.youtube.com/watch?v=3q3FV65JDBg",
+      docsLink: "https://help.figma.com/hc/en-us",
+      practiceLink: "https://www.figma.com/community"
     },
     {
-      "skillName": "UI/UX Principles",
-      "youtubeLink": "https://www.youtube.com/watch?v=sAQi_15-S2Y",
-      "docsLink": "https://lawsofux.com/",
-      "practiceLink": "https://sharpen.design/"
+      skillName: "UI/UX Principles",
+      youtubeLink: "https://www.youtube.com/watch?v=sAQi_15-S2Y",
+      docsLink: "https://lawsofux.com/",
+      practiceLink: "https://sharpen.design/"
     },
     {
-      "skillName": "Wireframing",
-      "youtubeLink": "https://www.youtube.com/watch?v=gJc2QPRO2E8",
-      "docsLink": "https://balsamiq.com/learn/articles/what-are-wireframes/",
-      "practiceLink": "https://wireframe.cc/"
+      skillName: "Wireframing",
+      youtubeLink: "https://www.youtube.com/watch?v=gJc2QPRO2E8",
+      docsLink: "https://balsamiq.com/learn/articles/what-are-wireframes/",
+      practiceLink: "https://wireframe.cc/"
     },
     {
-      "skillName": "Prototyping",
-      "youtubeLink": "https://www.youtube.com/watch?v=64sZ4L3wRxA",
-      "docsLink": "https://www.invisionapp.com/inside-design/what-is-prototyping/",
-      "practiceLink": "https://framer.com/projects"
+      skillName: "Prototyping",
+      youtubeLink: "https://www.youtube.com/watch?v=64sZ4L3wRxA",
+      docsLink: "https://www.invisionapp.com/inside-design/what-is-prototyping/",
+      practiceLink: "https://framer.com/projects"
     }
 ];
 
